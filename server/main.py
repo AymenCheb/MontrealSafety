95454f5ed18f06ffcab203096f48f4b70f1e7193
The0nlyOne <homernt@outlook.com>
2023-02-05 10:07:47 -0500

    Update main.py and Docker file Copy path

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
 from server.app import Server
-
+import os
 
 server = Server()
 app = server.create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
