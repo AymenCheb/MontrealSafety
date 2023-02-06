803474a16d3df85cb9136bcd9c9566400d446ec3
Aymen Chebbi <chebbi.aymen@hotmail.com>
2023-02-06 12:53:24 -0500

    Add waitress to the project

--- server/main.py
+++ server/main.py
@@ -6,4 +6,5 @@ app = server.create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
